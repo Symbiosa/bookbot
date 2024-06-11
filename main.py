6dfb4ddb6d7013e6effd84b5a21cbab3fb090d51
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-06-11 10:35:58 +0300

    Removed count function and tidied up some code

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from collections import Counter
-import pprint as pp
 #Opens the book and stores the string inside variable
 def main(): 
     with open("book/frankenstein.txt") as f: 
         fileContents = f.read()
         return fileContents
-#New function to split book and return word count
-def count():
-    words = main().split()
-    print(len(words))
 #function to count each character
 def chars():
     c = Counter(c for c in main().lower() if c.isalpha())
     for key, value in dict(c.most_common()).items():
         print(f"{key} decided to show up {value} times, yikes")
-    
 chars()
-    
